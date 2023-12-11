f89df18c38f30a8ea29b136f5544f167b468e625
LDF <liudengfeng_sd@outlook.com>
2023-12-11 10:50:37 +0800

    Fix sidebar issue and add logout check on page load

--- pages/04_🎧_听力练习.py
+++ pages/04_🎧_听力练习.py
@@ -6,14 +6,14 @@ from mypylib.streamlit_helper import check_and_force_logout
 
 # st.markdown("""<a href="page1" target="_self">page 1</a>""", unsafe_allow_html=True)
 
-sidebar_status = st.sidebar.empty()
-# 在页面加载时检查是否有需要强制退出的登录会话
-check_and_force_logout(st, sidebar_status)
-
 st.set_page_config(
     page_title="听力练习",
     page_icon="🎧",
     layout="wide",
 )
+
+sidebar_status = st.sidebar.empty()
+# 在页面加载时检查是否有需要强制退出的登录会话
+check_and_force_logout(st, sidebar_status)
 
 st.markdown("# 敬请期待......")
