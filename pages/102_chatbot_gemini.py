c96868437b3adab73a27195e3156c933bd1259aa
LDF <liudengfeng_sd@outlook.com>
2023-12-15 14:23:14 +0800

    Commented out unused code in 102_chatbot_gemini.py

--- pages/102_chatbot_gemini.py
+++ pages/102_chatbot_gemini.py
@@ -8,12 +8,12 @@ from mypylib.streamlit_helper import authenticate
 authenticate(st)
 
 
-def multiturn_generate_content():
-    config = {"max_output_tokens": 2048, "temperature": 0.9, "top_p": 1}
-    model = GenerativeModel("gemini-pro")
-    chat = model.start_chat()
-    response = chat.send_message("""你好""", generation_config=config)
-    st.write(response["usage_metadata"]["total_token_count"])  # type: ignore
+# def multiturn_generate_content():
+#     config = {"max_output_tokens": 2048, "temperature": 0.9, "top_p": 1}
+#     model = GenerativeModel("gemini-pro")
+#     chat = model.start_chat()
+#     response = chat.send_message("""你好""", generation_config=config)
+#     st.write(response["usage_metadata"]["total_token_count"])  # type: ignore
 
 
-multiturn_generate_content()
+# multiturn_generate_content()
