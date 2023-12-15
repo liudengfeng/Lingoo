f0d180cbcec2f138aa506a8d3b6ae934ead39b84
LDF <liudengfeng_sd@outlook.com>
2023-12-15 14:15:21 +0800

    Update 102_chatbot_gemini.py
    total_token_count

--- pages/102_chatbot_gemini.py
+++ pages/102_chatbot_gemini.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 import vertexai
 from vertexai.preview.generative_models import GenerativeModel, Part
-
+import time
 from mypylib.streamlit_helper import authenticate
 
 authenticate(st)
@@ -12,7 +12,8 @@ def multiturn_generate_content():
     config = {"max_output_tokens": 2048, "temperature": 0.9, "top_p": 1}
     model = GenerativeModel("gemini-pro")
     chat = model.start_chat()
-    st.write(chat.send_message("""介绍下你自己""", generation_config=config))
+    response = chat.send_message("""你好""", generation_config=config)
+    st.write(response["usage_metadata"]["total_token_count"])  # type: ignore
 
 
 multiturn_generate_content()
