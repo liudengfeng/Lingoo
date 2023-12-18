15ef3aa291de4a28d9b975ad7ccf45bf7ae24245
liudengfeng <liudengfeng_sd@outlook.com>
2023-12-18 10:31:19 +0800

    Update 102_chatbot_gemini.py
    t

--- pages/102_chatbot_gemini.py
+++ pages/102_chatbot_gemini.py
@@ -14,7 +14,7 @@ def multiturn_generate_content():
     chat = model.start_chat()
     response = chat.send_message("""你好""", generation_config=config)
     # st.write(response.usage_metadata.total_token_count)  # type: ignore
-    st.write(response)
+    st.write(response.candidates)
 
 
 multiturn_generate_content()
