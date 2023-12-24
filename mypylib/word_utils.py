ef6a7433d85c8d11bddd6d61ec8c8e900ee86389
liudengfeng <liudengfeng_sd@outlook.com>
2023-12-24 18:36:15 +0800

    Refactor audio_autoplay_elem function

--- mypylib/word_utils.py
+++ mypylib/word_utils.py
@@ -35,37 +35,6 @@ def get_word_cefr_map(name, fp):
         return json.load(f)
 
 
-# def audio_autoplay_elem(fp: str, controls: bool = False, fmt="mp3"):
-#     audio_type = "audio/mp3" if fmt == "mp3" else "audio/wav"
-#     with open(fp, "rb") as f:
-#         data = f.read()
-#         b64 = base64.b64encode(data).decode()
-#     if controls:
-#         return f"""\
-#             <audio controls autoplay>\
-#                 <source src="data:{audio_type};base64,{b64}" type="{audio_type}">\
-#                 Your browser does not support the audio element.\
-#             </audio>\
-#             <script>\
-#                 var audio = document.querySelector('audio');\
-#                 audio.load();\
-#                 audio.play();\
-#             </script>\
-#             """
-#     else:
-#         return f"""\
-#             <audio autoplay>\
-#                 <source src="data:{audio_type};base64,{b64}" type="{audio_type}">\
-#                 Your browser does not support the audio element.\
-#             </audio>\
-#             <script>\
-#                 var audio = document.querySelector('audio');\
-#                 audio.load();\
-#                 audio.play();\
-#             </script>\
-#             """
-
-
 def audio_autoplay_elem(data: Union[bytes, str], controls: bool = False, fmt="mp3"):
     audio_type = "audio/mp3" if fmt == "mp3" else "audio/wav"
 
@@ -99,37 +68,6 @@ def audio_autoplay_elem(data: Union[bytes, str], controls: bool = False, fmt="mp
     audio.play();\
 </script>\
             """
-
-
-# def audio_autoplay_elem(fp: str, controls: bool = False, fmt="mp3"):
-#     relative_path = os.path.relpath(fp, CURRENT_CWD)
-#     # 如果当前操作系统是 Windows，将反斜杠替换为正斜杠
-#     if os.name == "nt":
-#         relative_path = relative_path.replace("\\", "/")
-#     if controls:
-#         return f"""\
-#             <audio controls autoplay>\
-#                 <source src="{relative_path}" type="audio/mpeg">\
-#                 Your browser does not support the audio element.\
-#             </audio>\
-#             <script>\
-#                 var audio = document.querySelector('audio');\
-#                 audio.load();\
-#                 audio.play();\
-#             </script>\
-#             """
-#     else:
-#         return f"""\
-#             <audio autoplay>\
-#                 <source src="{relative_path}" type="audio/mpeg">\
-#                 Your browser does not support the audio element.\
-#             </audio>\
-#             <script>\
-#                 var audio = document.querySelector('audio');\
-#                 audio.load();\
-#                 audio.play();\
-#             </script>\
-#             """
 
 
 def gtts_autoplay_elem(text: str, lang: str, tld: str):
@@ -181,43 +119,6 @@ def sample_words(level, n):
     with open(fp, "r") as f:
         cefr = json.load(f)
     return random.sample(cefr[level], n)
-
-
-# def get_or_create_audio_in_blob_storage(word: str, style: str, secrets: dict):
-#     # 生成单词的哈希值
-#     hash_value = hash_word(word)
-
-#     # 生成单词的语音文件名
-#     filename = f"e{hash_value}.mp3"
-
-#     # 创建 BlobServiceClient 对象，用于连接到 Blob 服务
-#     blob_service_client = BlobServiceClient.from_connection_string(
-#         secrets["Microsoft"]["AZURE_STORAGE_CONNECTION_STRING"]
-#     )
-
-#     # 创建 ContainerClient 对象，用于连接到容器
-#     container_client = blob_service_client.get_container_client("word-voices")
-
-#     # 创建 BlobClient 对象，用于操作 Blob
-#     blob_client = container_client.get_blob_client(f"{style}/{filename}")
-
-#     # 如果 Blob 不存在，则调用 Azure 的语音合成服务生成语音文件，并上传到 Blob
-#     if not blob_client.exists():
-#         # 生成语音文件
-#         synthesize_speech_to_file(
-#             word,
-#             filename,
-#             secrets["Microsoft"]["SPEECH_KEY"],
-#             secrets["Microsoft"]["SPEECH_REGION"],
-#             style,  # type: ignore
-#         )
-
-#         # 上传文件到 Blob
-#         with open(filename, "rb") as data:
-#             blob_client.upload_blob(data)
-
-#     # 返回 Blob 的 URL
-#     return blob_client.url
 
 
 def get_or_create_and_return_audio_data(word: str, style: str, secrets: dict):
