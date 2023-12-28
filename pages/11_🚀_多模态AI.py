d20c26bfd455a041a6a4ccfeb0e366d63a623cd5
liudengfeng <liudengfeng_sd@outlook.com>
2023-12-29 06:23:02 +0800

    Add image display and file type warning in _process_media function

--- pages/11_🚀_多模态AI.py
+++ pages/11_🚀_多模态AI.py
@@ -127,10 +127,12 @@ check_and_force_logout(sidebar_status)
 def _process_media(uploaded_file):
     # 用文件扩展名称形成 MIME 类型
     mime_type = mimetypes.guess_type(uploaded_file.name)[0]
-    st.warning(f"文件类型：{mime_type}")
-    return Part.from_data(
+    p = Part.from_data(
         data=base64.b64decode(uploaded_file.getvalue()), mime_type=mime_type
     )
+    st.image(uploaded_file)
+    st.warning(f"文件类型：{p.mime_type}")
+    return p
 
 
 def _process_image_and_prompt(uploaded_files, prompt):
