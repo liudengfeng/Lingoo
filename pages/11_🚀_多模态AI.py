0c71a0c89d5d8929acaf973409a988601c691032
liudengfeng <liudengfeng_sd@outlook.com>
2023-12-31 17:13:31 +0800

    Update sidebar in AI.py

--- pages/11_🚀_多模态AI.py
+++ pages/11_🚀_多模态AI.py
@@ -44,8 +44,63 @@ if st.session_state.get("clear_prompt"):
 
 # endregion
 
-# region 共同边栏
+# region 边栏
 
+st.sidebar.markdown(
+    """:rainbow[运行设置]\n
+:gemini: 模型：gemini-pro-vision            
+"""
+)
+st.sidebar.slider(
+    "词元限制",
+    key="max_output_tokens",
+    min_value=32,
+    max_value=2048,
+    value=2048,
+    step=32,
+    help="""✨ 词元限制决定了一条提示的最大文本输出量。词元约为`4`个字符。默认值为`2048`""",
+)
+# 生成参数
+st.sidebar.slider(
+    "温度",
+    min_value=0.00,
+    max_value=1.0,
+    key="temperature",
+    value=0.0,
+    step=0.1,
+    help="✨ `temperature`（温度）可以控制词元选择的随机性。较低的温度适合希望获得真实或正确回复的提示，而较高的温度可能会引发更加多样化或意想不到的结果。如果温度为`0`，系统始终会选择概率最高的词元。对于大多数应用场景，不妨先试着将温度设为`0.2`。",
+)
+
+st.sidebar.slider(
+    "Top K",
+    key="top_k",
+    min_value=1,
+    max_value=40,
+    value=32,
+    step=1,
+    help="""✨ `Top-k`可更改模型选择输出词元的方式。
+- 如果`Top-k`设为`1`，表示所选词元是模型词汇表的所有词元中概率最高的词元（也称为贪心解码）。
+- 如果`Top-k`设为`3`，则表示系统将从`3`个概率最高的词元（通过温度确定）中选择下一个词元。
+- 多模态`Top-k`的默认值为`32`。""",
+)
+st.sidebar.slider(
+    "Top P",
+    key="top_p",
+    min_value=0.00,
+    max_value=1.0,
+    value=1.0,
+    step=0.05,
+    help="""✨ `Top-p`可更改模型选择输出词元的方式。系统会按照概率从最高到最低的顺序选择词元，直到所选词元的概率总和等于 Top-p 的值。
+- 例如，如果词元`A`、`B` 和`C`的概率分别是`0.3`、`0.2`和`0.1`，并且`Top-p`的值为`0.5`，则模型将选择`A`或`B`作为下一个词元（通过温度确定）。
+- 多模态`Top-p`的默认值为`1.0`。""",
+)
+
+st.sidebar.text_input(
+    "添加停止序列",
+    key="stop_sequences",
+    max_chars=64,
+    help="✨ 停止序列是一连串字符（包括空格），如果模型中出现停止序列，则会停止生成回复。该序列不包含在回复中。您最多可以添加五个停止序列。",
+)
 help_info = "✨ 对于 Gemini 模型，一个令牌约相当于 4 个字符。100 个词元约为 60-80 个英语单词。"
 sidebar_status = st.sidebar.empty()
 sidebar_status.markdown(
@@ -145,62 +200,6 @@ def generate_content_from_files_and_prompt(uploaded_files, prompt, response_cont
 # region 通用AI
 
 with tabs[0]:
-    st.sidebar.markdown(
-        """:rainbow[运行设置]\n
-:gemini: 模型：gemini-pro-vision            
-    """
-    )
-    st.sidebar.slider(
-        "词元限制",
-        key="max_output_tokens",
-        min_value=32,
-        max_value=2048,
-        value=2048,
-        step=32,
-        help="""✨ 词元限制决定了一条提示的最大文本输出量。词元约为`4`个字符。默认值为`2048`""",
-    )
-    # 生成参数
-    st.sidebar.slider(
-        "温度",
-        min_value=0.00,
-        max_value=1.0,
-        key="temperature",
-        value=0.0,
-        step=0.1,
-        help="✨ `temperature`（温度）可以控制词元选择的随机性。较低的温度适合希望获得真实或正确回复的提示，而较高的温度可能会引发更加多样化或意想不到的结果。如果温度为`0`，系统始终会选择概率最高的词元。对于大多数应用场景，不妨先试着将温度设为`0.2`。",
-    )
-
-    st.sidebar.slider(
-        "Top K",
-        key="top_k",
-        min_value=1,
-        max_value=40,
-        value=32,
-        step=1,
-        help="""✨ `Top-k`可更改模型选择输出词元的方式。
-- 如果`Top-k`设为`1`，表示所选词元是模型词汇表的所有词元中概率最高的词元（也称为贪心解码）。
-- 如果`Top-k`设为`3`，则表示系统将从`3`个概率最高的词元（通过温度确定）中选择下一个词元。
-- 多模态`Top-k`的默认值为`32`。""",
-    )
-    st.sidebar.slider(
-        "Top P",
-        key="top_p",
-        min_value=0.00,
-        max_value=1.0,
-        value=1.0,
-        step=0.05,
-        help="""✨ `Top-p`可更改模型选择输出词元的方式。系统会按照概率从最高到最低的顺序选择词元，直到所选词元的概率总和等于 Top-p 的值。
-- 例如，如果词元`A`、`B` 和`C`的概率分别是`0.3`、`0.2`和`0.1`，并且`Top-p`的值为`0.5`，则模型将选择`A`或`B`作为下一个词元（通过温度确定）。
-- 多模态`Top-p`的默认值为`1.0`。""",
-    )
-
-    st.sidebar.text_input(
-        "添加停止序列",
-        key="stop_sequences",
-        max_chars=64,
-        help="✨ 停止序列是一连串字符（包括空格），如果模型中出现停止序列，则会停止生成回复。该序列不包含在回复中。您最多可以添加五个停止序列。",
-    )
-
     st.header(":rocket: :rainbow[通用多模态AI]", divider="rainbow", anchor=False)
     st.markdown("""您可以向`Gemini`模型发送多模态提示信息。支持的模态包括文字、图片和视频。""")
 
