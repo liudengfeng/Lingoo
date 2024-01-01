4fcd173f98527aa528d3239986d70ddd6c8ff4f9
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-01 15:50:20 +0800

    Enable index creation for users collection in DbInterface

--- mypylib/db_interface.py
+++ mypylib/db_interface.py
@@ -59,12 +59,12 @@ class DbInterface:
         self.payments = self.db["payments"]
         self.words = self.db["words"]
         self.cache = TTLCache(maxsize=1000, ttl=86400)  # 24 hours cache
-        # self.users.create_indexes(
-        #     [
-        #         IndexModel([("phone_number", ASCENDING)], unique=True),
-        #         IndexModel([("email", ASCENDING)], unique=True),
-        #     ]
-        # )
+        self.users.create_indexes(
+            [
+                IndexModel([("phone_number", ASCENDING)], unique=True),
+                IndexModel([("email", ASCENDING)], unique=True),
+            ]
+        )
         self.payments.create_index(
             [
                 ("phone_number", ASCENDING),
