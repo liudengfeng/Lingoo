956fec383c3c4203e491dea7e6dc70d091f3d3b0
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-02 04:58:18 +0800

    Remove commented code and unused function

--- pages/04_🎧_听力练习.py
+++ pages/04_🎧_听力练习.py
@@ -6,7 +6,8 @@ from mypylib.st_utils import (
 )
 
 from mypylib.constants import CEFR_LEVEL_MAPS, rearrange_theme_scene
-from mypylib.google_vertex import generate_sub_scenes
+
+# from mypylib.google_vertex import generate_sub_scenes
 
 # "第二页"
 
@@ -31,8 +32,8 @@ if "sub_scenes" not in st.session_state:
 # region 辅助函数
 
 
-def on_theme_change():
-    st.session_state["sub_scenes"] = generate_sub_scenes(selected_theme, st)
+# def on_theme_change():
+#     st.session_state["sub_scenes"] = generate_sub_scenes(selected_theme, st)
 
 
 # endregion
@@ -46,8 +47,11 @@ check_and_force_logout(sidebar_status)
 # 添加 CEFR 分级选择框
 selected_cefr_level = st.sidebar.selectbox("选择你的 CEFR 分级", CEFR_LEVEL_MAPS.keys())
 # 根据选定的 CEFR 分级显示相应的场景主题
+# selected_theme = st.sidebar.selectbox(
+#     "选择场景主题", rearrange_theme_scene()[selected_cefr_level], on_change=on_theme_change
+# )
 selected_theme = st.sidebar.selectbox(
-    "选择场景主题", rearrange_theme_scene()[selected_cefr_level], on_change=on_theme_change
+    "选择场景主题", rearrange_theme_scene()[selected_cefr_level]
 )
 # 添加子场景选择框，可选列表为会话变量
 selected_sub_scene = st.sidebar.selectbox(
