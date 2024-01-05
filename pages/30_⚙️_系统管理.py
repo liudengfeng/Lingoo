1f746bdafd882499ebab330ac7a6370c21ef8da8
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-06 03:28:13 +0800

    Update save button label in mini dictionary editor

--- pages/30_⚙️_系统管理.py
+++ pages/30_⚙️_系统管理.py
@@ -888,7 +888,7 @@ with tabs[items.index("编辑微型词典")]:
 
     display_mini_dict_changes(df, view_elem)
 
-    if btn_cols[0].button("保存", key="save-btn-4", help="✨ 编辑简版词典，并保存到数据库"):
+    if btn_cols[0].button("保存", key="save-btn-4", help="✨ 将编辑后的简版词典变动部分保存到数据库"):
         save_changes_to_database(collection)
         st.session_state["mini_dict_df"]["edited_rows"] = {}
 
