939635306c57f2e910e9a98bd538ce1b4cc16be3
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-08 09:58:51 +0800

    Refactor import statements and add menu selection

--- pages/13_💪_练习.py
+++ pages/13_💪_练习.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-from mypylib.st_helper import check_access, configure_google_apis
+from mypylib.st_helper import (
+    check_access,
+    check_and_force_logout,
+    configure_google_apis,
+)
 
 # region 配置
 
@@ -12,7 +16,10 @@ st.set_page_config(
 
 check_access(False)
 configure_google_apis()
-
 # endregion
 
 menu_items = ["词汇练习", "口语练习", "听力练习", "阅读练习", "写作练习", "语法练习"]
+menu = st.sidebar.selectbox("菜单", menu_items, help="选择你要练习的项目")
+st.sidebar.divider()
+sidebar_status = st.sidebar.empty()
+check_and_force_logout(sidebar_status)
