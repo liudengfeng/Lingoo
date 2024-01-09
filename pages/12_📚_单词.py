ada18555ccc3c66c0721c2b42a06db037351bda3
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-09 17:06:37 +0800

    Update number of columns in pic_cols

--- pages/12_📚_单词.py
+++ pages/12_📚_单词.py
@@ -924,7 +924,7 @@ elif menu.endswith("看图测词"):
     st.markdown(
         "看图测词是一种记忆单词的游戏，其玩法是给出一个图片，玩家需要根据图片内容来猜测图片所代表的单词。这种游戏可以帮助玩家记忆单词的含义。需要注意的是，这个游戏只针对特定类别，例如只包括某个特定级别的词汇。如果你对某个领域不熟悉，由于需要根据图片来猜测单词，这个游戏的难度相对较大，可能需要投入更多的精力。我们建议只在你感兴趣的范围内尝试这个游戏。数据来源：[Cambridge Dictionary](https://dictionary.cambridge.org/)"
     )
-    pic_cols = st.columns(4)
+    pic_cols = st.columns(2)
     category = pic_cols[0].selectbox("请选择图片类别", pic_categories)
     pic_num = pic_cols[1].number_input("请选择图片测词考题数量", 1, 20, value=10, step=1)
 
