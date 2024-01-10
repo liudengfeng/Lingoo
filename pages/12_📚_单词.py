076f88a69c802e5d0e84deea2223aa018ca5a0ac
liudengfeng <liudengfeng_sd@outlook.com>
2024-01-10 13:03:23 +0800

    Fix on_change function in view_pic_question

--- pages/12_📚_单词.py
+++ pages/12_📚_单词.py
@@ -630,12 +630,12 @@ def view_pic_question():
         index=user_prev_answer_idx,
         label_visibility="collapsed",
         key="pic_options",
-        # on_change=on_pic_radio_change,
-        # args=(idx,),
+        on_change=on_pic_radio_change,
+        args=(idx,),
     )
     container.write(f"idx: {idx} 用户选择答案：{user_answer}")
     # 保存用户答案
-    st.session_state.user_pic_answer[idx] = user_answer
+    st.session_state.user_pic_answer[idx] = user_prev_answer
     container.write(f"显示 idx: {idx} 用户答案：<{st.session_state.user_pic_answer}>")
 
 
